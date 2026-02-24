5efe167ad173a95fa07fbaa51aa469aa697b0e0b
madortill <mador.till@gmail.com>
2026-02-24 21:57:15 +0200

    update 12

--- src/components/Roadmap/Roadmap.jsx
+++ src/components/Roadmap/Roadmap.jsx
@@ -1,4 +1,5 @@
-import "./Roadmap.css";
+import React from "react";
+import styles from "./Roadmap.module.css";
 import pinkPlanet from "../../assets/img/planets/pink-planet.svg";
 import greenPlanet from "../../assets/img/planets/green-planet.svg";
 import purplePlanet from "../../assets/img/planets/purple-planet.svg";
@@ -6,25 +7,31 @@ import bluePlanet from "../../assets/img/planets/blue-planet.svg";
 
 const Roadmap = ({ onSelectPlanet }) => {
   return (
-    <div id="planets">
-      <div className="planet pink" onClick={() => onSelectPlanet("pink")}>
-        <img src={pinkPlanet} alt="pink planet" />
-        <p>מבוא</p>
-      </div>
+    <div className={styles.container}>
+      <div className={styles.mainWrapper}>
+        <div className={styles.mapArea}>
 
-      <div className="planet green" onClick={() => onSelectPlanet("green")}>
-        <img src={greenPlanet} alt="green planet" />
-        <p>הגדרה</p>
-      </div>
+          <div className={`${styles.planet} ${styles.pink}`} onClick={() => onSelectPlanet("pink")}>
+            <img src={pinkPlanet} alt="pink" />
+            <p>מבוא</p>
+          </div>
 
-      <div className="planet purple" onClick={() => onSelectPlanet("purple")}>
-        <img src={purplePlanet} alt="purple planet" />
-        <p>סוגי אסטרואידים</p>
-      </div>
+          <div className={`${styles.planet} ${styles.green}`} onClick={() => onSelectPlanet("green")}>
+            <img src={greenPlanet} alt="green" />
+            <p>הגדרה</p>
+          </div>
 
-      <div className="planet blue" onClick={() => onSelectPlanet("blue")}>
-        <img src={bluePlanet} alt="blue planet" />
-        <p>פיצוץ אסטרואידים</p>
+          <div className={`${styles.planet} ${styles.purple}`} onClick={() => onSelectPlanet("purple")}>
+            <img src={purplePlanet} alt="purple" />
+            <p>סוגי אסטרואידים</p>
+          </div>
+
+          <div className={`${styles.planet} ${styles.blue}`} onClick={() => onSelectPlanet("blue")}>
+            <img src={bluePlanet} alt="blue" />
+            <p>פיצוץ אסטרואידים</p>
+          </div>
+
+        </div>
       </div>
     </div>
   );
