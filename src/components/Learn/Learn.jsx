8b798f0cfe22684de2b256cd4dc4e595f68650bc
madortill <mador.till@gmail.com>
2026-02-24 20:36:07 +0200

    update 11

--- src/components/Learn/Learn.jsx
+++ src/components/Learn/Learn.jsx
@@ -1,24 +1,43 @@
-import "./Learn.css";
+import React from "react";
+import styles from "./Learn.module.css";
 import pinkPlanet from "../../assets/img/planets/pink-planet.svg";
 import Astronaut from "../../assets/img/astronaut.svg";
+import backBtn from "../../assets/img/back-btn.svg";
 
 const Learn = ({ onBack }) => {
     return (
-        <div className="learn-intro">
-            <button className="back-button" onClick={onBack}>
-                חזור
-            </button>
-            <h1 className="subj-header">מבוא -
-                למה זה חשוב?</h1>
-            <p className="text">
-                שיעור פיצוץ אסטרואיד ידוע בחשיבותו בגלל הסכנה היומיומית הנשקפת לכדור הארץ בלעדיו.
-                עשרות אלפי אסטרואידים נעים לכיוון הכוכב שלנו, ועליכן לדעת כיצד למנוע מהם להגיע אליו.
-            </p>
-            <img src={Astronaut} alt="Astronaut Dani" id="dani-learn"/>
-            <button className="button next-button" onClick={onBack}>
-                הבנתי!
-            </button>
-            <img src={pinkPlanet} alt="pink planet" id="pink-half"/>
+        <div className={styles.container}>
+            <div className={styles.mainWrapper}>
+
+                <img
+                    src={backBtn}
+                    alt="back"
+                    className={styles.backButton}
+                    onClick={onBack}
+                />
+
+                <main className={styles.mainContent}>
+                    <h1 className={styles.subjHeader}>
+                        מבוא - למה זה חשוב?
+                    </h1>
+
+                    <p className={styles.text}>
+                        שיעור פיצוץ אסטרואיד ידוע בחשיבותו בגלל הסכנה היומיומית הנשקפת לכדור הארץ בלעדיו.
+                        עשרות אלפי אסטרואידים נעים לכיוון הכוכב שלנו, ועליכן לדעת כיצד למנוע מהם להגיע אליו.
+                    </p>
+
+                    <div className={styles.visualGroup}>
+                        <img src={Astronaut} alt="Astronaut Dani" className={styles.daniLearn} />
+                        <div className={styles.planetContainer}>
+                            <img src={pinkPlanet} alt="pink planet" className={styles.pinkHalf} />
+                        </div>
+                    </div>
+                </main>
+
+                <button className={`${styles.button} ${styles.nextButton}`} onClick={onBack}>
+                    הבנתי!
+                </button>
+            </div>
         </div>
     );
 };
