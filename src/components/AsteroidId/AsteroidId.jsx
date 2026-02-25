4f2699240b7d8c8d4b3a484599028b24b9c3b6bb
madortill <mador.till@gmail.com>
2026-02-25 12:01:48 +0200

    update 15

--- src/components/AsteroidId/AsteroidId.jsx
+++ src/components/AsteroidId/AsteroidId.jsx
@@ -1,24 +1,41 @@
-import "./AsteroidId.css";
+import React from "react";
+import styles from "./AsteroidId.module.css";
 import purplePlanet from "../../assets/img/planets/purple-planet.svg";
 import AstronautHi from "../../assets/img/astronaut-saying-hi.svg";
+import backBtn from "../../assets/img/back-btn.svg";
 
 const AsteroidId = ({ asteroid, onBack, onFinish }) => {
   return (
-    <div className="asteroid-id">
-      <button className="back-button" onClick={onBack}>חזור</button>
+    <div className={styles.container}>
+      <div className={styles.mainWrapper}>
+        <header className={styles.header}>
+          <img src={backBtn} alt="back" className={styles.backButton} onClick={onBack} />
+        </header>
 
-      <h1 className="subj-header">סוגי אסטרואידים - מול מי אנחנו נלחמים?</h1>
+        <main className={styles.mainContent}>
+          <h1 className={styles.subjHeader}>סוגי אסטרואידים - מול מי אנחנו נלחמים?</h1>
 
-      <div className="id-div">
-        <h2 id="header-id">{asteroid.title}</h2>
-        <p className="text" id="text-id">{asteroid.text}</p>
-        <img src={asteroid.image} alt={asteroid.image} id="grown-astroid" />
+          <div className={styles.visualGroup}>
+            <div className={styles.idCard}>
+              <h2 className={styles.cardTitle}>{asteroid.title}</h2>
+              <p className={styles.cardText}>{asteroid.text}</p>
+              <img src={asteroid.image} alt={asteroid.title} className={styles.grownAsteroid} />
+            </div>
+
+            <img src={AstronautHi} alt="Astronaut Dani" className={styles.daniHi} />
+
+            <div className={styles.planetContainer}>
+              <img src={purplePlanet} alt="purple planet" className={styles.purpleHalf} />
+            </div>
+
+            <div className={styles.footer}>
+              <button className={styles.nextButton} onClick={onBack}>
+                הבנתי!
+              </button>
+            </div>
+          </div>
+        </main>
       </div>
-
-      {onFinish && <button className="button next-button left" onClick={onFinish}>חזרה לחללית</button>}
-
-      <img src={AstronautHi} alt="Astronaut Dani saying hi" id="dani-definition-right" />
-      <img src={purplePlanet} alt="purple planet" id="purple-half" className="half-planet" />
     </div>
   );
 };
