c7f234c978a0e94a71f2a9d7139305fcff120cb3
Tzeela Belmaker <mador.till.mop@gmail.com>
2026-03-09 16:55:45 +0200

    home 1

--- src/components/End/End.jsx
+++ src/components/End/End.jsx
@@ -1,23 +1,18 @@
-import "./End.css";
+import styles from "./End.module.css";
 import { useNavigate } from "react-router-dom";
 import DaniEnd from "../../assets/img/astronaut.svg";
-import bluePlanet from "../../assets/img/planets/blue-planet.svg"
+import bluePlanet from "../../assets/img/planets/blue-planet.svg";
 
 const End = () => {
     const navigate = useNavigate();
 
     return (
-        <div id="end">
-            <h1 className="subj-header">כל הכבוד, סיימת
-                את לומדת
-                מלחמת אסטרואידים!
-                בזכותך כדור הארץ ניצל</h1>
-            <img src={DaniEnd} alt="dani end page" id="dani-end" />
-            <p className="end-mini-text">החומר לא יושב לכם עד הסוף?</p>
-            <button className="button next-button" onClick={() => navigate("/")} >
-                מעבר על הלומדה פעם נוספת
-            </button>
-            <img src={bluePlanet} alt="blue planet" id="blue-half" className="half-planet" />
+        <div className={styles.endContainer}>
+            <h1 className={styles.subjHeader}>כל הכבוד, סיימת את לומדת מלחמת אסטרואידים! בזכותך כדור הארץ ניצל</h1>
+            <img src={DaniEnd} alt="dani end page" className={styles.daniEnd} />
+            <p className={styles.endMiniText}>החומר לא יושב לכם עד הסוף?</p>
+            <button className={styles.nextButton} onClick={() => navigate("/")}>מעבר על הלומדה פעם נוספת</button>
+            <img src={bluePlanet} alt="blue planet" className={`${styles.halfPlanet} half-planet`} />
         </div>
     );
 };
